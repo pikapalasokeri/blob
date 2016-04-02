bba0ac3ca431d77a841a3969a61124efe1d39f9b
pikapalasokeri <devnull@localhost>
2016-04-02 20:55:26 +0200

    Converting image to simple gray.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 
-print "derp"
+from scipy import misc
+import numpy as np
+
+def rgb2gray(img):
+  return np.dot(img[...,:3], [1.0/3.0, 1.0/3.0, 1.0/3.0])
+
+def getBlobs(filePath):
+  img = misc.imread(filePath)
+  grayImg = rgb2gray(img)
+  
+
+if __name__ == "__main__":
+  getBlobs("images/1.jpg")
+  
