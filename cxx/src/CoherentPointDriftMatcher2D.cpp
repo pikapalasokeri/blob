e1f0aadc95e8d9bfc2a5050f28f1eed865f6692f
pikapalasokeri <devnull@localhost>
2016-07-07 19:49:52 +0200

    Removing stupid cxx api and putting everything in one nice file.

--- cxx/src/CoherentPointDriftMatcher2D.cpp
+++ cxx/src/CoherentPointDriftMatcher2D.cpp
@@ -1,5 +1,36 @@
 #include "CoherentPointDriftMatcher2D.hpp"
-#include "../CoherentPointDriftMatcher2DImpl.hpp"
+
+#include <iostream>
+
+CoherentPointDriftMatcher2D::CoherentPointDriftMatcher2D()
+{
+  std::cout << "Constructing." << std::endl;
+}
+
+CoherentPointDriftMatcher2D::~CoherentPointDriftMatcher2D()
+{
+  std::cout << "Destroying." << std::endl;
+}
+
+
+void
+CoherentPointDriftMatcher2D::addPoint1(double x, double y)
+{
+
+}
+
+void
+CoherentPointDriftMatcher2D::addPoint2(double x, double y)
+{
+
+}
+
+void
+CoherentPointDriftMatcher2D::match()
+{
+
+}
+
 
 extern "C" {
   CoherentPointDriftMatcher2D* CoherentPointDriftMatcher2D_new()
--- cxx/src/CoherentPointDriftMatcher2D.hpp
+++ cxx/src/CoherentPointDriftMatcher2D.hpp
@@ -1,7 +1,28 @@
 #ifndef CoherentPointDriftMatcher2D_hpp
 #define CoherentPointDriftMatcher2D_hpp
 
-class CoherentPointDriftMatcher2D;
+#include <vector>
+#include <utility>
+
+class CoherentPointDriftMatcher2D
+{
+public:
+  CoherentPointDriftMatcher2D();
+
+  ~CoherentPointDriftMatcher2D();
+
+  void addPoint1(double x, double y);
+
+  void addPoint2(double x, double y);
+
+  void match();
+
+private:
+  typedef std::pair<double, double> DoublePair;
+  typedef std::vector<DoublePair> PointVector;
+  PointVector pointSet1_;
+  PointVector pointSet2_;
+};
 
 extern "C" {
   CoherentPointDriftMatcher2D* CoherentPointDriftMatcher2D_new();
