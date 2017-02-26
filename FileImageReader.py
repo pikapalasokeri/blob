149c9f8a01ff2398554228e8c75b80ae8ac308c2
pikapalasokeri <devnull@localhost>
2017-02-26 12:23:35 +0100

    FileImageReader can now read image paths from config file.

--- FileImageReader.py
+++ FileImageReader.py
@@ -1,12 +1,39 @@
 from ReferenceImage import ReferenceImage
 from scipy import misc
 
+
+def _getKeyboardInputNext():
+    inputStr = raw_input("Path to image (leave empty when done): ")
+    if not inputStr:
+        return None
+    
+    filePath = inputStr
+    comment = raw_input("Comment: ")
+    return ReferenceImage(misc.imread(filePath), comment)
+
+
 class FileImageReader:
+    def __init__(self, configFile = None):
+        self._imagesFromConfigFile = []
+        self._indexInConfigFileImages = 0
+        if configFile is not None:
+            with open(configFile) as config:
+                for line in config:
+                    filePath = line.strip()
+                    self._imagesFromConfigFile.append(ReferenceImage(misc.imread(filePath), filePath))
+    
     def getNext(self):
-        inputStr = raw_input("Path to image (leave empty when done): ")
-        if not inputStr:
+        if len(self._imagesFromConfigFile) == 0:
+            return _getKeyboardInputNext()
+        else:
+            return self._getNextInConfigFile()
+
+
+    def _getNextInConfigFile(self):
+        if self._indexInConfigFileImages == len(self._imagesFromConfigFile):
             return None
-        
-        filePath = inputStr
-        comment = raw_input("Comment: ")
-        return ReferenceImage(misc.imread(filePath), comment)
+
+        self._indexInConfigFileImages += 1
+        return self._imagesFromConfigFile[self._indexInConfigFileImages-1]
+
+
--- createConfig.py
+++ createConfig.py
@@ -3,7 +3,7 @@
 from ConfigCreator import ConfigCreator
 from FileImageReader import FileImageReader
 
-imageReader = FileImageReader()
+imageReader = FileImageReader("images/early_tests_white_01.config")
 creator = ConfigCreator(imageReader)
 
 creator.readImages()
