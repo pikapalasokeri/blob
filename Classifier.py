7442cd276bb1f3ec455a53b6d10c74e7dd5d6445
pikapalasokeri <devnull@localhost>
2017-04-09 10:46:59 +0200

    Using SimulatedAnnealingPointMatcher in Classifier.

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
 from EdgeDetector import EdgeDetector
-from cxx.CxxPointMatcher import CxxCoherentPointDriftMatcher2D
+from cxx.CxxSimulatedAnnealingPointMatcher import CxxSimulatedAnnealingPointMatcher2D
 from CoherentPointDriftMatcher import transform
 
 class Classifier:
@@ -8,7 +8,7 @@ class Classifier:
         self._references = references
         self._edgeDetectionConfig = edgeDetectionConfig
         self._referenceEdges = [self._detectEdges(x) for x in references]
-        
+
     def classify(self, image):
         edgesToClassify = self._detectEdges(image)
         bestScore = float("Inf")
@@ -20,7 +20,7 @@ class Classifier:
             print "    ", reference.comment, "   ", score
             if score is None:
                 continue
-            
+
             if score < bestScore:
                 bestScore = score
                 currentGuess = reference
@@ -36,24 +36,24 @@ class Classifier:
         return _convertToMatrix(trimmedEdges)
 
     '''
-    I really don't know why this method works so well, except for threshold 0.8 or 0.2, 
+    I really don't know why this method works so well, except for threshold 0.8 or 0.2,
     but I'm keeping it since it seems to work very well for everything else,
     and 0.8 and 0.2 are off by 2 at most.
     '''
     def _trimEdges(self, points):
         newPoints = []
-        
+
         keepThreshold = self._edgeDetectionConfig.keepFactor
         accumulatedKeep = 0.0
         accumulatedKeepCorrection = 0.0
         for point in points:
             if accumulatedKeep-accumulatedKeepCorrection < keepThreshold:
-                newPoints.append(point)        
+                newPoints.append(point)
         accumulatedKeep += keepThreshold
-        
+
         if accumulatedKeep - accumulatedKeepCorrection >= 1.0:
             accumulatedKeepCorrection += 1.0
-            
+
         return newPoints
 
 def _convertToMatrix(points):
@@ -64,42 +64,43 @@ def _convertToMatrix(points):
     return newPoints
 
 def _getLikenessScore(reference, edgesToClassify):
-    matcher = CxxCoherentPointDriftMatcher2D()
-    matcher.setW(0.1)
-    matcher.setMaxIterations(500)
-    matcher.setMinIterations(10)
-    matcher.setSigmaSquareChangeTolerance(0.01)
-    matcher.setVerbose(False)
+    referenceCenter = np.mean(reference, axis = 0)
+    edgesToClassifyCenter = np.mean(edgesToClassify, axis = 0)
+    centeredReference = reference - referenceCenter
+    centeredEdgesToClassify = edgesToClassify - edgesToClassifyCenter
 
-    for p in edgesToClassify:
-        matcher.addPoint1(p[0], p[1])    
+    matcher = CxxSimulatedAnnealingPointMatcher2D()
+    matcher.setNumIterations(2000)
 
-    for p in reference:
+    for p in centeredEdgesToClassify:
+        matcher.addPoint1(p[0], p[1])
+
+    for p in centeredReference:
         matcher.addPoint2(p[0], p[1])
 
     scale, rotation, translation = matcher.match()
     if scale < 0.8 or scale > 1.2:
         return None
-    
-    transformed = transform(scale, rotation, translation, edgesToClassify)
-    return _computeClosestNeighborLikeness(reference, transformed)
+
+    transformed = transform(scale, rotation, translation, centeredEdgesToClassify)
+    return _computeClosestNeighborLikeness(centeredReference, transformed)
 
 def _computeClosestNeighborLikeness(points1, points2):
-  squareDistances = _computeSquareDistances(points1, points2)
-  from1ClosestNeighbor = np.min(squareDistances, axis=1)
-  from2ClosestNeighbor = np.min(squareDistances, axis=0)
-  return np.mean(from1ClosestNeighbor) + np.mean(from2ClosestNeighbor)
+    squareDistances = _computeSquareDistances(points1, points2)
+    from1ClosestNeighbor = np.min(squareDistances, axis=1)
+    from2ClosestNeighbor = np.min(squareDistances, axis=0)
+    return np.mean(from1ClosestNeighbor) + np.mean(from2ClosestNeighbor)
 
 def _computeSquareDistances(points1, points2):
-  numPoints1 = points1.shape[0]
-  numPoints2 = points2.shape[0]
+    numPoints1 = points1.shape[0]
+    numPoints2 = points2.shape[0]
 
-  squareDistances = np.zeros((numPoints1, numPoints2))
-  for i, point1 in enumerate(points1):
-    for j, point2 in enumerate(points2):
-      squareDistances[i,j] = _computeSquareDistance(point1, point2)
-  return squareDistances
+    squareDistances = np.zeros((numPoints1, numPoints2))
+    for i, point1 in enumerate(points1):
+        for j, point2 in enumerate(points2):
+            squareDistances[i,j] = _computeSquareDistance(point1, point2)
+    return squareDistances
 
 def _computeSquareDistance(point1, point2):
-  diff = point1 - point2
-  return float(diff[0]**2 + diff[1]**2)
+    diff = point1 - point2
+    return float(diff[0]**2 + diff[1]**2)
