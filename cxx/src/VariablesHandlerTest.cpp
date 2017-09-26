f883265045329bcbd7f3bc204a6879b255092e03
pikapalasokeri <none>
2017-09-26 05:27:36 +0200

    Cxx unit tests no longer stops after first failed assertion.

--- cxx/src/VariablesHandlerTest.cpp
+++ cxx/src/VariablesHandlerTest.cpp
@@ -19,9 +19,9 @@ TEST_CASE("Default best","[VariablesHandler]")
   translation << -1.0, -1.0;
 
   handler.getBest(scale, rotation, translation);
-  REQUIRE(scale == 1.0);
-  REQUIRE(rotation == Matrix2d::Identity(2, 2));
-  REQUIRE(translation == MatrixXd::Zero(1, 2));
+  CHECK(scale == 1.0);
+  CHECK(rotation == Matrix2d::Identity(2, 2));
+  CHECK(translation == MatrixXd::Zero(1, 2));
 }
 
 TEST_CASE("Single proposeNewVariables", "[VariablesHandler]")
@@ -33,15 +33,17 @@ TEST_CASE("Single proposeNewVariables", "[VariablesHandler]")
 
   handler.proposeNewVariables(scale, rotation, translation);
 
-  REQUIRE(scale == 1.0);
+  CHECK(scale == 1.0);
 
-  REQUIRE(isRotationMatrix(rotation));
+  CHECK(isRotationMatrix(rotation));
   Matrix2d expectedRotation;
   expectedRotation <<  0.911943, 0.410316,
                       -0.410316, 0.911943;
-  REQUIRE(closeEnough(rotation, expectedRotation));
+  CHECK(closeEnough(rotation, expectedRotation));
 
   TranslationVector expectedTranslation;
   expectedTranslation << -12.015, -4.37855;
-  REQUIRE(closeEnough(translation, expectedTranslation));
+  CHECK(closeEnough(translation, expectedTranslation));
 }
+
+
