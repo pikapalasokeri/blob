f0ea7e37f8a539ce810da5503a0583c3397f0ecf
pikapalasokeri <noemail>
2017-10-29 11:47:11 +0100

    DemoClassifier: adding flag to profile. Running on new data.

--- DemoClassifier.py
+++ DemoClassifier.py
@@ -1,35 +1,53 @@
+#!/usr/bin/python
+
 from Classifier import Classifier
 from EdgeDetectionConfig import EdgeDetectionConfig
 from FileImageReader import FileImageReader
+import sys
 
-config = EdgeDetectionConfig(2.2, 6.5, 30, 1.0)
-referenceImageReader = FileImageReader("images/early_tests_white_01.config")
-referenceImages = [x for x in referenceImageReader.generate()]
+def runDemo():
+    config = EdgeDetectionConfig(2, 2, 40, 1.0)
+    #referenceImageReader = FileImageReader("../demoseries/clean_demo_series_6_crop.config")
+    referenceImageReader = FileImageReader("../demoseries/clean_demo_series_6_crop_small.config")
+    referenceImages = [x for x in referenceImageReader.generate()]
+    print "Reference images generated."
 
-classifier = Classifier(config, referenceImages)
+    classifier = Classifier(config, referenceImages)
+    print "Classifier created."
 
-# just try to classify the references as sanity check.
-totalTries = 0
-numCorrect = 0
-allImagesReader = FileImageReader("images/early_tests_white_all.config")
-for image in allImagesReader.generate():
-    classified = classifier.classify(image)
-    if classified is not None:
-        firstPart = image.comment.split("_")[-2]
-        secondPart = classified.comment.split("_")[-2]
-        if firstPart == secondPart:
-            correct = True
-            numCorrect += 1
+    # just try to classify the references as sanity check.
+    totalTries = 0
+    numCorrect = 0
+    allImagesReader = FileImageReader("../demoseries/clean_demo_series_6_crop.config")
+    #allImagesReader = FileImageReader("../demoseries/clean_demo_series_6_crop_small.config")
+    for image in allImagesReader.generate():
+        classified = classifier.classify(image)
+
+        if classified is not None:
+            firstFileName = image.comment.split("/")[-1]
+            firstPart = firstFileName.split("_")[0]
+            secondFileName = classified.comment.split("/")[-1]
+            secondPart = secondFileName.split("_")[0]
+            if firstPart == secondPart:
+                correct = True
+                numCorrect += 1
+            else:
+                correct = False
+            print "     Classified", image.comment, "as", classified.comment, "   ", correct
         else:
-            correct = False
-        print "     Classified", image.comment, "as", classified.comment, "   ", correct
+            print "     Completely failed to classify", image.comment
+        totalTries += 1
+        print ""
+
+    print "Total tries:", totalTries
+    print "Correct classifications:", numCorrect
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        runDemo()
+    elif sys.argv[1] == "profile":
+        print "Running through cProfile..."
+        import cProfile
+        cProfile.run(runDemo())
     else:
-        print "     Completely failed to classify", image.comment
-    totalTries += 1
-    print ""
-
-
-
-
-print "Total tries:", totalTries
-print "Correct classifications:", numCorrect
+        "Unrecognized argument."
