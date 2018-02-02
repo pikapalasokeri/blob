8aa9090d3274f82c2b25084f3e256363f07c1107
pikapalasokeri <noemail>
2018-02-02 22:03:31 +0100

    Removing old comment.

--- src/main/cxx/Random.hpp
+++ src/main/cxx/Random.hpp
@@ -21,23 +21,4 @@ private:
   std::mt19937_64 mersenneTwisterEngine_;
 };
 
-/*
-class StandardNormalDistributionNumberGenerator
-{
-public:
-  StandardNormalDistributionNumberGenerator()
-    : mersenneTwisterEngine_(0761306430)
-  {}
-
-  double getNext()
-  {
-    return distribution_(mersenneTwisterEngine_);
-  }
-
-private:
-  std::normal_distribution<double> distribution_;
-  std::mt19937_64 mersenneTwisterEngine_;
-};
-*/
-
 #endif
