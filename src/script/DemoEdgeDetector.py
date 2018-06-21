7a095a74033bc0112129356a0dc0ae606ac6c3d9
pikapalasokeri <noemail>
2018-06-21 23:29:44 +0200

    Fix flake8 errors in DemoEdgeDetector

--- src/script/DemoEdgeDetector.py
+++ src/script/DemoEdgeDetector.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
 
 from scipy import misc
-from scipy import ndimage
-import numpy as np
 import matplotlib.pyplot as plt
-from EdgeDetector import *
+from EdgeDetector import EdgeDetector
+
 
 def addEdgesToImage(image, edges, colorIx):
-  for point in edges:
-    row = point[0]
-    col = point[1]
-    image[int(row), int(col), colorIx] = 255
+    for point in edges:
+        row = point[0]
+        col = point[1]
+        image[int(row), int(col), colorIx] = 255
 
 if __name__ == "__main__":
-  filePath = "../../images/TryEdgeDetector.jpg"
-  img = misc.imread(filePath)
-  edgeDetector = EdgeDetector(img)
-  radius = 1000
+    filePath = "../../images/TryEdgeDetector.jpg"
+    img = misc.imread(filePath)
+    edgeDetector = EdgeDetector(img)
+    radius = 1000
 
+    sigma = 4
+    thresholdFactor = 0.4
+    edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
+    print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
+    addEdgesToImage(img, edges, 0)
+    addEdgesToImage(img, edges, 2)
 
-  sigma = 4
-  thresholdFactor = 0.4
-  edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
-  print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
-  addEdgesToImage(img, edges, 0)
-  addEdgesToImage(img, edges, 2)
+    sigma = 8
+    thresholdFactor = 0.1
+    edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
+    print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
+    addEdgesToImage(img, edges, 1)
 
-  sigma = 8
-  thresholdFactor = 0.1
-  edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
-  print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
-  addEdgesToImage(img, edges, 1)
+    sigma = 16
+    thresholdFactor = 0.01
+    edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
+    print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
+    addEdgesToImage(img, edges, 2)
 
-  sigma = 16
-  thresholdFactor = 0.01
-  edges = edgeDetector.getEdges(sigma, thresholdFactor, radius)
-  print("Got", edges.size(), "edge points at sigma", sigma, "and thresholdFactor", thresholdFactor)
-  addEdgesToImage(img, edges, 2)
-
-  plt.figure()
-  plt.imshow(img)
-  plt.show()
+    plt.figure()
+    plt.imshow(img)
+    plt.show()
