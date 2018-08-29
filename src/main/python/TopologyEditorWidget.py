ceb6d5878b203bca0411b04665a45baa10a972e1
pikapalasokeri <noemail>
2018-08-29 21:16:58 +0200

    Parse json and extract node names

--- src/main/python/TopologyEditorWidget.py
+++ src/main/python/TopologyEditorWidget.py
@@ -1,56 +1,79 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPlainTextEdit, QListWidget
 from PyQt5.QtCore import QObject, pyqtSignal
+import json
 
 
 class Model(QObject):
-    modelUpdated = pyqtSignal(int)
+    modelUpdated = pyqtSignal(list)
 
     def __init__(self):
         super().__init__()
+        self._currentNodes = []
 
-    def tryUpdate(self):
+    def tryUpdate(self, maybeJsonText):
         print("Trying update...")
-        success = True
-        if success:
-            self.modelUpdated.emit(-123)
+        print(maybeJsonText)
+        try:
+            validJson = json.loads(maybeJsonText)
+            if type(validJson) == list:
+                nodeNames = []
+                for element in validJson:
+                    print("element:", element)
+                    if "name" in element:
+                        nodeNames.append(element["name"])
+                print("validJson:")
+                print(validJson)
+                self.modelUpdated.emit(nodeNames)
+        except json.JSONDecodeError as e:
+            print(str(e))
+
+
+class JsonTextEdit(QPlainTextEdit):
+    newTextChanged = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.textChanged.connect(self.newTextChangedProxy)
+
+    def newTextChangedProxy(self):
+        self.newTextChanged.emit(self.toPlainText())
 
 
 class JsonEditorWidget(QWidget):
+    textChangedWithText = pyqtSignal(str)
+
     def __init__(self, parent, model):
         super().__init__(parent)
-        editor = QPlainTextEdit(self)
+        editor = JsonTextEdit(self)
         editor.setStyleSheet("QPlainTextEdit{font-family: Courier New;}")
         editor.resize(400, 700)
 
-        editor.textChanged.connect(model.tryUpdate)
+        editor.newTextChanged.connect(model.tryUpdate)
 
 
 class NodeListWidget(QWidget):
-    def __init__(self, parent, model):
+    def __init__(self, parent):
         super().__init__(parent)
-        nodeList = QListWidget(self)
-        nodeList.resize(200, 400)
-        self._model = model
+        self._nodeList = QListWidget(self)
+        self._nodeList.resize(200, 400)
 
     def update(self, nodeNames):
         print("Updating node list...")
         # read shit from model and update this list
         print(nodeNames)
-
-
-def printSomething():
-    print("printSomething:")
+        self._nodeList.clear()
+        self._nodeList.addItems(nodeNames)
 
 
 class TopologyEditorWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
 
-        model = Model()
-        jsonEditor = JsonEditorWidget(self, model)
-        nodeList = NodeListWidget(self, model)
+        self._model = Model()
+        jsonEditor = JsonEditorWidget(self, self._model)
+        nodeList = NodeListWidget(self)
 
-        model.modelUpdated.connect(nodeList.update)
+        self._model.modelUpdated.connect(nodeList.update)
 
         grid = QGridLayout()
         grid.addWidget(nodeList, 0, 0)
