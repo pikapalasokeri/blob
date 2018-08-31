cb5f94628c8c94aaa36b1b77ac94d93ddb53da0f
pikapalasokeri <noemail>
2018-08-31 22:41:07 +0200

    Replace image grid layout with image table view

--- src/main/python/ImageGridWidget.py
+++ src/main/python/ImageGridWidget.py
@@ -1,28 +1,47 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout
-from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QWidget, QTableView, QHeaderView
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QVariant, QAbstractTableModel
+from PyQt5 import QtCore
 
 
-class DummyWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawRect(0, 0, 280, 280)
+IMAGE_SIZE = 280
 
 
-def addImagesToGrid(grid):
-    for i in range(0, 2):
-        for j in range(0, 2):
-            dummyImage = DummyWidget()
-            grid.addWidget(dummyImage, j, i)
+class ImageTableModel(QAbstractTableModel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def rowCount(self, parent):
+        return 2
+
+    def columnCount(self, parent):
+        return 2
+
+    def data(self, index, role):
+        filePath = "/home/pikapalasokeri/blob/images/TryEdgeDetector.jpg"
+        image = QImage(filePath)
+        pixmap = QPixmap(image.scaledToWidth(IMAGE_SIZE))
+        if role == QtCore.Qt.DecorationRole:
+            print("Returning pixmap")
+            print(pixmap)
+            return pixmap
+        if role == QtCore.Qt.SizeHintRole:
+            return pixmap.size()
+
+        return QVariant()
 
 
 class ImageGridWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
-        imageGrid = QGridLayout()
-
-        addImagesToGrid(imageGrid)
-
-        self.setLayout(imageGrid)
+        self._imageTable = QTableView(self)
+        model = ImageTableModel(self)
+        self._imageTable.setModel(model)
+        self._imageTable.resize(600, 800)
+        self._imageTable.setShowGrid(False)
+        self._imageTable.horizontalHeader().setDefaultSectionSize(IMAGE_SIZE)
+        self._imageTable.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self._imageTable.horizontalHeader().setVisible(False)
+        self._imageTable.verticalHeader().setDefaultSectionSize(IMAGE_SIZE)
+        self._imageTable.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self._imageTable.verticalHeader().setVisible(False)
