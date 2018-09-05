0e1e9000ecc4c58229c2737b6226a85e53926765
pikapalasokeri <noemail>
2018-09-05 21:34:10 +0200

    Speed up qImageToMatrix

--- src/main/python/ImageTableModel.py
+++ src/main/python/ImageTableModel.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import QVariant, QAbstractTableModel
-from PyQt5.QtGui import QPixmap, QColor, QImage, qRed, qGreen, qBlue
+from PyQt5.QtGui import QPixmap, QColor, QImage
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 import numpy as np
@@ -14,12 +14,11 @@ def qImageToMatrix(inData):
     height = inData.height()
     width = inData.width()
     outData = np.zeros((height, width, 3))
-    for row in range(0, height):
-        for col in range(0, width):
-            argb = inData.pixel(col, row)
-            outData[row, col, :] = [qRed(argb),
-                                    qGreen(argb),
-                                    qBlue(argb)]
+    bytesString = inData.constBits().asstring(inData.byteCount())
+    tmp = np.fromstring(bytesString, dtype=np.uint8).reshape((height, width, 4))
+    outData[:, :, 2] = tmp[:, :, 0]
+    outData[:, :, 1] = tmp[:, :, 1]
+    outData[:, :, 0] = tmp[:, :, 2]
     print("-QImage to matrix")
     return outData
 
